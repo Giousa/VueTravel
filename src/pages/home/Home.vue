49d64ec78b18023e39b0fba2e9417a787ddc7a49
Giousa <65489469@qq.com>
2018-09-25 23:08:16 +0800

    路由功能

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+    <div class="home">Home首页</div>
+
+    <router-link to="/list">列表页</router-link>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+  name: 'Home'
+}
+
+</script>
+
+<style scoped>
+
+  .home{
+    font-size: 30px;
+    color: #990000;
+  }
+
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>List列表</div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'List'
+}
+
+</script>
+
+<style scoped>
+
+</style>
